5314d958a4e47c76afad09ebe5e35e5f62b336f4
612491005 <suman.5.ghosh@bt.com>
2020-06-27 13:55:35 +0530

    Updates

---
+++ precache-manifest.fbb305f339fbd6e04f953f3a413e409a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bd0e5f1cc2785e7b5410ad4e65b9ee",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7dc4a41123d11159273f",
+    "url": "/monsters-rolodex/static/css/main.ffc35be1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc4a41123d11159273f",
+    "url": "/monsters-rolodex/static/js/main.45505d1d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
